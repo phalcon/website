68e8c58a792704f83b796b29d844b6d4785fc213
Phalcon <andres@phalconphp.com>
2012-05-14 14:05:17 -0500

    Support for IE

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,10 +1,20 @@
 
-if(navigator.userAgent.toLowerCase().indexOf("win")!=-1){
-	document.getElementById("fork").style.display = "none";
+function initIndex(){
+	var github = document.getElementById("github");
+	if(github){
+		if(navigator.userAgent.toLowerCase().indexOf("win")!=-1){
+			github.style.display = "none";
+		} else {
+			github.style.width = "250px";
+		}
+	};
+	var s = document.createElement('SCRIPT');
+	s.src = "http://speakerdeck.com/embed/4edf11f72a2b980050009919.js";
+	document.getElementById("speakerdeck").appendChild(s);
 }
 
-window.onload = function(){
-        var s = document.createElement('SCRIPT');
-        s.src = "http://speakerdeck.com/embed/4edf11f72a2b980050009919.js";
-        document.getElementById("speakerdeck").appendChild(s);
-};
+if(!document.all){
+	document.addEventListener('DOMContentLoaded', initIndex);
+} else {
+	window.onload = initIndex;
+}
