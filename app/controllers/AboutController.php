361810ba664dd616a575297f711220a71229e434
Phalcon <andres@phalconphp.com>
2012-05-22 19:41:50 -0500

    Adding about page

---
+++ app/controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AboutController extends ControllerBase {
+
+	public function initialize(){
+		$this->view->setTemplateAfter('main');
+		Phalcon_Tag::setTitle('About');
+		parent::initialize();
+	}
+
+	public function indexAction(){
+
+	}
+
+}
