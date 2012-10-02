fb7bc5222f822aa0669f9e5861ba9f3ed54b23eb
Nikolaos Dimopoulos <nikos@niden.net>
2012-10-02 19:34:14 -0400

    Download: Reformatted the area to automate link generation 	modified:   app/controllers/DownloadController.php 	modified:   app/views/download/index.phtml 	new file:   public/img/download30x30.png 	new file:   public/img/github30x30.png 	new file:   public/img/pear32x32.gif

--- app/controllers/DownloadController.php
+++ app/controllers/DownloadController.php
@@ -11,50 +11,100 @@ class DownloadController extends ControllerBase
 
     public function indexAction()
     {
-        $files = array(
-            'win-050-g-539-x86-vc9'     => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &gt; 5.3.9 (VC9)',
-                                            'file' => 'files/phalcon-win-x86-php5.3-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
-            'win-050-l-539-x86-vc9'     => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &lt;= 5.3.9 (VC9)',
-                                            'file' => 'files/phalcon-win-x86-php5.3.9-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
-            'win-050-g-539-x86-vc9-nts' => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &gt; 5.3.9 NTS (VC9)',
-                                            'file' => 'files/phalcon-win-x86-php5.3-nts-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
-            'win-050-g-539-x64-vc9'     => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x64 for PHP &gt; 5.3.9 (VC9)',
-                                            'file' => 'files/phalcon-win-x64-php5.3-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
-            'win-050-l-539-x86-vc9'     => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x64 for PHP &lt;= 5.3.9 (VC9)',
-                                            'file' => 'files/phalcon-win-x64-php5.3.9-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
-            'win-050-e-540-x86-vc9'     => array(
-                                            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP 5.4 (VC9)',
-                                            'file' => 'files/phalcon-win-x86-php5.4-0.5-VC9.zip',
-                                            'date' => '',
-                                           ),
+        $dll_01 = array(
+            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP 5.4 (VC9)',
+            'file' => 'files/phalcon-win-x86-php5.4-0.5-VC9.zip',
+            'date' => '',
         );
 
+        $dll_02 = array(
+            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &gt; 5.3.9 (VC9)',
+            'file' => 'files/phalcon-win-x86-php5.3-0.5-VC9.zip',
+            'date' => '',
+        );
+
+        $dll_03 = array(
+            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &gt; 5.3.9 NTS (VC9)',
+            'file' => 'files/phalcon-win-x86-php5.3-nts-0.5-VC9.zip',
+            'date' => '',
+        );
+//        $dll_04 = array(
+//            'name' => 'Phalcon 0.5.0 - Windows x86 for PHP &lt;= 5.3.9 (VC9)',
+//            'file' => '',
+//            'date' => '',
+//        );
+//
+//        $dll_10 = array(
+//            'name' => 'Phalcon 0.5.0 - Windows x64 for PHP 5.4 (VC9)',
+//            'file' => '',
+//            'date' => '',
+//        );
+
+        $dll_11 = array(
+            'name' => 'Phalcon 0.5.0 - Windows x64 for PHP &gt; 5.3.9 (VC9)',
+            'file' => 'http://phalconphp.com/files/phalcon-win-x64-php5.3-0.5-VC9.zip',
+            'date' => '',
+        );
+
+        $current = array(
+            'x86' => array($dll_01, $dll_02, $dll_03),
+            'x64' => array($dll_11),
+        );
+//        $current = array(
+//            'x86' => array($dll_01, $dll_02, $dll_03, $dll_04),
+//            'x64' => array($dll_10, $dll_11),
+//        );
+
         // Get the last modified date of each file
-        foreach ($files as $key => $file) {
+        foreach ($current as $versionKey => $version) {
 
-            $fileName = $file['file'];
-            $fileDate = filemtime($fileName);
+            foreach ($version as $key => $file) {
+                $fileName = $file['file'];
+                $fileDate = filemtime($fileName);
 
-            if ($fileDate) {
-                $files[$key]['date'] = date("F d Y H:i:s", $fileDate);
+                if ($fileDate) {
+                    $current[$versionKey][$key]['date'] = date("F d Y H:i:s", $fileDate);
+                }
             }
         }
 
-        $this->view->setVar('files', $files);
+        $dll_01 = array(
+            'name' => 'Phalcon 0.4.5 - Windows x86 for PHP 5.4 (VC9)',
+            'file' => 'files/phalcon-win-x86-php5.4-0.4-VC9.zip',
+            'date' => '',
+        );
+
+        $dll_02 = array(
+            'name' => 'Phalcon 0.4.5 - Windows x86 for PHP 5.3 (VC9)',
+            'file' => 'files/phalcon-win-x86-php5.3-0.4-VC9.zip',
+            'date' => '',
+        );
+
+        $dll_10 = array(
+            'name' => 'Phalcon 0.4.5 - Windows x64 for PHP 5.3 (VC9)',
+            'file' => 'files/phalcon-win-x64-php5.3-0.4-VC9.zip',
+            'date' => '',
+        );
+
+        $old = array(
+            'x86' => array($dll_01, $dll_02),
+            'x64' => array($dll_10),
+        );
+
+        // Get the last modified date of each file
+        foreach ($old as $versionKey => $version) {
+
+            foreach ($version as $key => $file) {
+                $fileName = $file['file'];
+                $fileDate = filemtime($fileName);
+
+                if ($fileDate) {
+                    $old[$versionKey][$key]['date'] = date("F d Y H:i:s", $fileDate);
+                }
+            }
+        }
+
+        $this->view->setVar('current', $current);
+        $this->view->setVar('old', $old);
     }
 }
