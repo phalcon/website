f114490fda7987374c6b42c585b88c9339230f7f
Nikolaos Dimopoulos <nikos@phalconphp.com>
2013-03-22 15:34:53 -0400

    Added hosting/consulting sections 	new file:   app/controllers/ConsultingController.php 	new file:   app/controllers/HostingController.php 	new file:   app/views/consulting/index.volt 	new file:   app/views/hosting/index.volt 	modified:   app/views/partials/sidebar.volt 	modified:   public/css/style.css

---
+++ app/controllers/ConsultingController.php
@@ -0,0 +1,14 @@
+<?php
+
+class ConsultingController extends \Ph\Controller
+{
+    public function initialize()
+    {
+        \Phalcon\Tag::setTitle('Consulting');
+        parent::initialize();
+    }
+
+    public function indexAction()
+    {
+    }
+}
