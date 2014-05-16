19a19355ebacb54407148b09e2440e787d7a1a68
Дмитрий Пацура <zaets28rus@gmail.com>
2014-05-16 09:40:38 +0400

    UtilsController more beautiful

--- app/controllers/UtilsController.php
+++ app/controllers/UtilsController.php
@@ -46,36 +46,42 @@ EOF;
 
         $repositories = array(
             'cphalcon' => 4,
+            'zephir' => 4,
             'docs' => 2.5,
             'website' => 2.5,
             'phalcon-devtools' => 2,
-            'invo' => 0.9,
-            'album-o-rama' => 0.9,
-            'zephir' => 4,
-            'vokuro' => 1,
-            'forum' => 1.5,
             'incubator' => 2,
             'zephir-docs' => 1.5,
+            'forum' => 1.5,
+            'vokuro' => 1,
             'php-phalcon' => 1.0,
+            'invo' => 0.9,
+            'album-o-rama' => 0.9,
             'php-site' => 0.9,
             'mvc' => 0.9,
             'dasshy' => 0.9
         );
+
         $c = array();
         $p = array();
         $l = array();
+
         foreach ($repositories as $repository => $weight) {
-            $contributorsJson = file_get_contents('https://api.github.com/repos/phalcon/' . $repository . '/contributors', false, $context);
-            $d = json_decode($contributorsJson, true);
-            if (is_array($d)) {
-                foreach ($d as $contributor) {
-                    $login = $contributor['login'];
-                    if (!isset($c[$login])) {
-                        $c[$login] = 0;
-                        $p[$login] = $contributor['avatar_url'];
-                        $l[$login] = $contributor['html_url'];
+            $responce = file_get_contents('https://api.github.com/repos/phalcon/' . $repository . '/contributors', false, $context);
+            if ($responce) {
+                $contributors = json_decode($responce, true);
+
+                if (is_array($contributors)) {
+                    foreach ($contributors as $contributor) {
+                        $login = $contributor['login'];
+                        
+                        if (!isset($c[$login])) {
+                            $c[$login] = 0;
+                            $p[$login] = $contributor['avatar_url'];
+                            $l[$login] = $contributor['html_url'];
+                        }
+                        $c[$login] += ($contributor['contributions'] * $weight);
                     }
-                    $c[$login] += ($contributor['contributions'] * $weight);
                 }
             }
         }
