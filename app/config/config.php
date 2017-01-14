facd81dc05ef1c1edb74ada419f68fbd020449fa
Nikolaos Dimopoulos <nikos@niden.net>
2017-01-14 14:33:16 -0500

    Added google Analytics

--- app/config/config.php
+++ app/config/config.php
@@ -2,23 +2,26 @@
 
 defined('APP_PATH') || define('APP_PATH', realpath('.'));
 
-return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
-        'charset'     => 'utf8',
-    ),
-    'application' => array(
-        'controllersDir' => APP_PATH . '/app/controllers/',
-        'modelsDir'      => APP_PATH . '/app/models/',
-        'migrationsDir'  => APP_PATH . '/app/migrations/',
-        'viewsDir'       => APP_PATH . '/app/views/',
-        'pluginsDir'     => APP_PATH . '/app/plugins/',
-        'libraryDir'     => APP_PATH . '/app/library/',
-        'cacheDir'       => APP_PATH . '/app/cache/',
-        'baseUri'        => '/test/',
-    )
-));
+return new \Phalcon\Config(
+    [
+        'database' => [
+            'adapter'     => 'Mysql',
+            'host'        => 'localhost',
+            'username'    => 'root',
+            'password'    => '',
+            'dbname'      => 'test',
+            'charset'     => 'utf8',
+        ],
+        'application' => [
+            'controllersDir'  => APP_PATH . '/app/controllers/',
+            'modelsDir'       => APP_PATH . '/app/models/',
+            'migrationsDir'   => APP_PATH . '/app/migrations/',
+            'viewsDir'        => APP_PATH . '/app/views/',
+            'pluginsDir'      => APP_PATH . '/app/plugins/',
+            'libraryDir'      => APP_PATH . '/app/library/',
+            'cacheDir'        => APP_PATH . '/app/cache/',
+            'baseUri'         => '/test/',
+            'googleAnalytics' => 'UA-90300500-2',
+        ],
+    ]
+);
