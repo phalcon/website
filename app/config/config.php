a7c9cc040a5b7996f25108401eac99d6342d2f87
Nikolaos Dimopoulos <nikos@niden.net>
2017-04-06 16:19:44 -0400

    Updated version

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@ $downloadSlugs = 'linux|windows|tools|docker|stubs';
 
 return [
     'app'           => [
-        'version'         => '3.1.1',
+        'version'         => '3.1.2',
         'timezone'        => getenv('APP_TIMEZONE'),
         'debug'           => getenv('APP_DEBUG'),
         'env'             => getenv('APP_ENV'),
